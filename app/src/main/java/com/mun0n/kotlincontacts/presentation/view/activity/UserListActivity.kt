ecb98629f12100693b620cebd9097511885a548d
Javi Muñoz <munozmunozjavier@gmail.com>
2018-01-04 16:10:12 +0100

    First commit

---
+++ app/src/main/java/com/mun0n/kotlincontacts/presentation/view/activity/UserListActivity.kt
@@ -0,0 +1,13 @@
+package com.mun0n.kotlincontacts.presentation.view.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.mun0n.kotlincontacts.R
+
+class UserListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_list)
+    }
+}
